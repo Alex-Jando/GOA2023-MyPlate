e83d46938f718b53dd25020fb4b1fc5d015759f7
Alex Jando <alexjando2007@outlook.com>
2023-04-18 19:26:50 -0700

    Commit V0.0.1
    Created first version of a webview. Setup package, and even made a very
    simple website.
    
    TODO: Make the code read an html file to get the code instead of writing
    it in plain text.

--- app/src/main/java/com/example/mealprepapp/MainActivity.kt
+++ app/src/main/java/com/example/mealprepapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.mealprepapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupWebView()
+    }
+    // Local code will be ran, so there is no chance for an XXS attack, and javascript doesn't pose a threat
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        val webView: WebView = findViewById(R.id.webview)
+
+        webView.webViewClient = WebViewClient()
+
+        webView.settings.javaScriptEnabled = true
+
+        val webViewData = "<html><body><h1 style=\"text-align: center;\">Hello Alex!</h1></body</html>"
+
+        val encodedWebViewData = Base64.encodeToString(webViewData.toByteArray(), Base64.NO_PADDING)
+
+        webView.loadData(encodedWebViewData,
+                         "text/html",
+                         "base64")
     }
 }
