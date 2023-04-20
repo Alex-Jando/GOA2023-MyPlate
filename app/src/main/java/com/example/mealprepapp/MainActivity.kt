4c48b82e5c89721c4e0c3fd4675b5b803467b5f9
BuildTools <unconfigured@null.spigotmc.org>
2023-04-19 21:46:52 -0700

    V0.0.4
    First functioning product is complete. It still is missing all the
    features, but the kotlin code is done. We can now start working on the
    design using html, css, and javascript. A lot has changed from the last
    commit. Now instead of searching each file, and reading it's data, the
    files are handled by the webClient. Also I added an icon for the time
    being, although, it could use some improvement.
    
    IMPORTANT TO NOTE:
    - When using html, css, or javascript, you need to reference objects in
      the following way:
    
      1. If the object is code, store it in the assets folder, and reference
         it with "/assets/{filename}"
      2. If the object is an img, video, sound, etc... then store it in the
         ressources folder, and reference it with
         "/res/{ressourceType}/{filename}"

--- app/src/main/java/com/example/mealprepapp/MainActivity.kt
+++ app/src/main/java/com/example/mealprepapp/MainActivity.kt
@@ -3,50 +3,42 @@ package com.example.mealprepapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
-import android.webkit.WebViewClient
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupWebView()
-    }
+
     // Local code will be ran, so there is no chance for an XXS attack, and javascript doesn't pose a threat
     @SuppressLint("SetJavaScriptEnabled")
-    private fun setupWebView() {
-        val webView: WebView = findViewById(R.id.webview)
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-        webView.webViewClient = CustomWebViewClient()
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        
+        val webView: WebView = findViewById(R.id.webview)
 
         webView.settings.javaScriptEnabled = true
 
-        loadFileData("index.html")
-    }
-    private fun getAssetData(fileName: String): String {
-        val fileStream = application.assets.open(fileName).bufferedReader()
-        val fileData = fileStream.use {
-            it.readText()
-        }
-        return fileData
-    }
-    private fun loadFileData(fileName: String) {
-        val webView: WebView = findViewById(R.id.webview)
-        val data = getAssetData(fileName)
-        webView.loadData(data,
-                         "text/html",
-                         "utf-8")
+        val assetLoader = WebViewAssetLoader.Builder()
+                        .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+                        .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
+                        .build()
+
+        webView.webViewClient = LocalContentWebViewClient(assetLoader)
+
+        webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
     }
 }
 
-private class CustomWebViewClient : WebViewClient() {
-    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-        return if url.startsWith("App://") {
-            loadFileData(url)
-            false // Allow the WebView to load the URL instead of opening it in a browser
-        } else {
-            true
-        }
+private class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
+    override fun shouldInterceptRequest(
+        view: WebView,
+        request: WebResourceRequest
+    ): WebResourceResponse? {
+        return assetLoader.shouldInterceptRequest(request.url)
     }
-    
 }
