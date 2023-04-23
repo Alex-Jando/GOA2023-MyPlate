7a72e4162514367d2faa5ef614a0944726520383
Alex Jando <alexjando2007@outlook.com>
2023-04-22 20:17:53 -0700

    V0.0.7
    Added javascript <-> kotlin interface.

--- app/src/main/java/com/example/mealprepapp/MainActivity.kt
+++ app/src/main/java/com/example/mealprepapp/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.mealprepapp
 
 import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import android.widget.Toast
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
-    // All JS ran, will be handled by verified by the app, so there shouldn't be any security concerns.
+    // All Javascript ran, will be handled by verified by the app, so there shouldn't be any security concerns.
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,6 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         webView.settings.javaScriptEnabled = true
 
+        webView.addJavascriptInterface(WebAppInterface(this), "app")
+
         val assetLoader = WebViewAssetLoader.Builder()
                         .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
                         .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
@@ -31,6 +38,21 @@ class MainActivity : AppCompatActivity() {
         webView.webViewClient = LocalContentWebViewClient(assetLoader)
 
         webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
+    }
+}
+
+class WebAppInterface(private val mContext: Context) {
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+    }
+    @JavascriptInterface
+    fun getAsset(fileName: String): String {
+        return mContext.assets.open(fileName).bufferedReader().use { it.readText() }
+    }
+    @JavascriptInterface
+    fun saveMealPlans(mealPlans: String): String {
+        return mealPlans
     }
 }
 
